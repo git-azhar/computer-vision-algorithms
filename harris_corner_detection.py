971fd6405708e6c1469363ce84f02acd141a6e3c
Zuzee00 <2muhammadazhar@gmail.com>
2020-09-13 23:49:08 +0500

    added get harris_points method

--- harris_corner_detection.py
+++ harris_corner_detection.py
@@ -22,3 +22,37 @@ def compute_harris_response(image, sigma: int = 5) -> float:
     W_trace = W_xx + W_yy
 
     return W_det / W_trace
+
+
+def get_harris_points(harris_image, min_dist: int = 10, threshold: float = 0.1):
+    """
+    :param harris_image: harris response image
+    :param min_dist:  minimum number of pixels separating
+                        corners and image boundary
+    :param threshold:
+    :return:  Return corners
+    """
+
+    # find top corner candidate above a threshold
+    corner_thresh = harris_image.max() * threshold
+    harris_image_t = (harris_image > corner_thresh) * 1
+
+    # get coordinates of corner candidate & their values
+    coordinates = np.array(harris_image_t.nonzero()).T
+    candidate_values = [harris_image[c[0], c[1]] for c in coordinates]
+
+    # sort
+    index = np.argsort(candidate_values)
+
+    # store allowed position in array
+    allowed_positions = np.zeros(harris_image.shape)
+    allowed_positions[min_dist:-min_dist, min_dist:-min_dist] = 1
+
+    # select best points taking min_dist into account
+    filtered_coords = []
+    for i in index:
+        if allowed_positions[coordinates[i, 0], coordinates[i, 1]] == 1:
+            filtered_coords.append(coordinates[i])
+            allowed_positions[(coordinates[i, 0] - min_dist): (coordinates[i, 1] + min_dist),
+             (coordinates[i, 1] - min_dist):(coordinates[i, 1] + min_dist)] = 0
+    return filtered_coords
